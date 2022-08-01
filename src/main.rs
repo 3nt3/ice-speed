c767696f112f241dda787e10800fac432a62519f
3nt3 <gott@3nt3.de>
2022-08-01 16:43:38 +0200

    added data structures
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{serde::ts_milliseconds, serde::ts_milliseconds_option, DateTime, Utc};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -5,6 +6,67 @@ use serde::Deserialize;
 struct TrainStatus {
     speed: f32,
     train_type: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct TripResponse {
+    trip: Trip,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Trip {
+    stops: Vec<Stop>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Stop {
+    station: Station,
+    timetable: Timetable,
+    info: StopInfo,
+    track: Track,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Station {
+    eva_nr: String,
+    name: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Timetable {
+    #[serde(with = "ts_milliseconds_option")]
+    scheduled_arrival_time: Option<DateTime<Utc>>,
+    #[serde(with = "ts_milliseconds_option")]
+    actual_arrival_time: Option<DateTime<Utc>>,
+    arrival_delay: String,
+    #[serde(with = "ts_milliseconds_option")]
+    scheduled_departure_time: Option<DateTime<Utc>>,
+    #[serde(with = "ts_milliseconds_option")]
+    actual_departure_time: Option<DateTime<Utc>>,
+    show_actual_departure_time: Option<bool>,
+    departure_delay: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct StopInfo {
+    status: i32,
+    passed: bool,
+    position_status: String,
+    distance: i32,
+    distance_from_start: i32,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Track {
+    scheduled: String,
+    actual: String,
 }
 
 #[tokio::main]
@@ -22,5 +84,21 @@ async fn main() {
         .json::<TrainStatus>()
         .await
         .unwrap_or_else(|why| panic!("error deserializing: {}", why));
+
+    let trip = client
+        .get("https://iceportal.de/api1/rs/tripInfo/trip")
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0",
+        )
+        .send()
+        .await
+        .unwrap()
+        .json::<TripResponse>()
+        .await
+        .unwrap_or_else(|why| panic!("error deserializing trip info: {}", why));
+
+    dbg!(trip);
+
     println!("{}: {} km/h", status.train_type, status.speed);
 }
